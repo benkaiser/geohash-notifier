ff51b9ac815e6dc3f3655209c00e3b54c3ada7ca
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-10-27 21:37:48 +1000

    use pug for templates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 app.use(require('./controllers'));
 
 app.listen(process.env.PORT || 3000, function() {
