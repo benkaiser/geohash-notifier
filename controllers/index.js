56dac3a649e28441f82d724d016831f81f7a1d27
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2016-10-27 19:12:05 +1000

    add basic form for creating subscription

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send('test');
+  res.sendFile('form.html', { root: 'views' });
 });
 
 module.exports = router;
